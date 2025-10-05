696aa6322fa4bd40bf6b0bfcbcde14bf547b9782
BUNN1E5 <bunn1e5@users.noreply.github.com>
2025-10-05 16:13:42 -0700

    Forgot to save, boids AI startign implementation

--- entities/sheep/sheep_ai.gd
+++ entities/sheep/sheep_ai.gd
@@ -5,30 +5,48 @@ class_name SheepAI
 
 #Potentially a dictionary with an array might be better
 #Something for later
-static var sheep : Array[SheepAI]
+static var all_sheep : Array[SheepAI]
 
 #One must imaging a spherical sheep
 var sheep_diameter : float
+var speed : float
 static var alignment : float
 static var separation : float 
 static var cohesion : float #Cohesion is gonna start high
+static var max_speed : float
 
 var sight : float
 var min_flock_size : int
 var hunger : float #This will determine Grazing behaviour
 var thirst : float #Grazing behaviour but near water
 
-func 
+func _process(delta):
+	return
 
-#This is technically 2D BOIDS lmao
-func separation_calculation():
-	return;
+#Good layout of the rules are here:
+#https://people.engr.tamu.edu/sueda/courses/CSCE450/2023F/projects/Frank_Martinez/index.html
+func cohesion_calculation():
+	if len(all_sheep) == 0:
+		return Vector3(0,0,0)
+	
+	var center_of_mass = Vector3(0,0,0)
+	var avg_speed = 0
+	for sheep in all_sheep:
+		center_of_mass += sheep.position
+		avg_speed += sheep.speed;
+		break
+	center_of_mass /= len(all_sheep)
+	var cohesion_vector = center_of_mass - position
+	return cohesion_vector * avg_speed;
 
 func alignment_calculation():
 	return;
 
-func cohesion_calculation():
+#This is technically 2D BOIDS lmao
+func separation_calculation():
+	
 	return;
+
 
 func predator_calculation():
 	return;
