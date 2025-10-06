1a85f3d36df422bf8f0d9aacde7245f08ef0d173
BUNN1E5 <bunn1e5@users.noreply.github.com>
2025-10-06 01:23:44 -0700

    WHY IS BOIDS NOT WORK

--- entities/sheep/sheep_manager.gd
+++ entities/sheep/sheep_manager.gd
@@ -3,15 +3,20 @@ class_name SheepManager
 
 var sheep_prefab = preload("res://entities/sheep/sheep.tscn")
 
-var alignment : float
-var separation : float 
-var cohesion : float #Cohesion is gonna start high
-var max_speed : float
+@export var alignment : float
+@export var separation : float 
+@export var cohesion : float #Cohesion is gonna start high
+@export var max_speed : float
+@export var friction : float
+@export var max_force : float = 10.
+
+var gravity : float = -9.81
 
 var all_sheep : Array
 var sheep_at_position : Dictionary
 var center_of_mass : Vector3
 var average_velocity : Vector3
+
 
 var names_available : Array
 static var _i : SheepManager
@@ -22,18 +27,18 @@ func _ready() -> void:
 	if(error == OK):
 		names_available = json.data
 	_i = self
-	for i in range(5):
-		var sheep = sheep_prefab.instantiate()
+	for i in range(1):
+		var sheep = sheep_prefab.instantiate() as SheepAI
 		add_child(sheep)
+		sheep.position = Vector3(randf(), randf(), randf()) * sheep.sheep_diameter*2 * (i+1)
 		if(!names_available.is_empty()):
-			var index = randi_range(0, len(names_available))
+			var index = randi_range(0, len(names_available)-1)
 			sheep.name = "sheep | " + names_available[index]
 			names_available.remove_at(index)
 	pass
 
 func _process(delta: float) -> void:
 	calculate_globals()
-	return
 
 #Good layout of the rules are here:
 #https://people.engr.tamu.edu/sueda/courses/CSCE450/2023F/projects/Frank_Martinez/index.html
@@ -44,8 +49,9 @@ func calculate_globals():
 		center_of_mass += sheep.position
 	center_of_mass /= len(all_sheep)
 	average_velocity /= len(all_sheep)
-	print("center of mass : " + str(center_of_mass))
-	print("average_velocity : " + str(average_velocity))
+	DebugDraw3D.draw_sphere(center_of_mass, .5, Color.RED)
+	DebugDraw3D.draw_line(center_of_mass, center_of_mass + average_velocity)
+	
 	return
 	
 
